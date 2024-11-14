08acc9c06451fbd1497b8be0a2d6c62f5723488a
amine_benmbark@hotmail.com <dortmund91000@hotmail.com>
2024-11-14 16:27:31 +0100

    sz

--- serveur.js
+++ serveur.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+// Connexion à MongoDB (remplace <url_mongodb> par ton URL MongoDB)
 mongoose.connect('mongodb://localhost:27017/nombdd', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -10,5 +11,8 @@ mongoose.connect('mongodb://localhost:27017/nombdd', {
 .then(() => console.log('Connexion MongoDB réussie'))
 .catch((error) => console.error('Erreur de connexion à MongoDB :', error));
 
-
+// Middleware pour traiter les données JSON
 app.use(express.json());
+
+
+
