795c76722c30cba64c779fdd3f8288409b9cecbd
Lamrini-asmae <[200~lamrini.asmae01@gmail.com>
2024-11-14 16:48:07 +0100

    rootgetbyid

--- serveur.js
+++ serveur.js
@@ -12,3 +12,13 @@ mongoose.connect('mongodb://localhost:27017/nombdd', {
 
 
 app.use(express.json());
+// Lire un élément par ID
+app.get('/items/:id', async (req, res) => {
+    try {
+        const item = await Item.findById(req.params.id);
+        if (!item) return res.status(404).json({ message: "Élément non trouvé" });
+        res.json(item);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
