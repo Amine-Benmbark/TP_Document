604e5055cad96d65cdce2044d66cbb0803320f63
Lamrini-asmae <[200~lamrini.asmae01@gmail.com>
2024-11-14 16:53:59 +0100

    rootgetbyid

--- serveur.js
+++ serveur.js
@@ -50,3 +50,13 @@ app.delete('/items/:id', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+// Lire un élément par ID
+app.get('/items/:id', async (req, res) => {
+    try {
+        const item = await Item.findById(req.params.id);
+        if (!item) return res.status(404).json({ message: "Élément non trouvé" });
+        res.json(item);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
